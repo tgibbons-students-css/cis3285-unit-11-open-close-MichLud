7b193539bca83c2fa0ba529f9514338513bdb969
Michael Ludwig <mludwig2@css.edu>
2024-11-17 12:53:05 -0600

    Assignment 11 Completion - Does not run in my version of VS Code

--- AbstractTrader/TradeProcessorVersion1.cs
+++ AbstractTrader/TradeProcessorVersion1.cs
@@ -10,7 +10,25 @@ namespace AbstractTrader
 {
     public class TradeProcessorVersion1 : TradeProcessor
     {
-        protected override IEnumerable<string> ReadTradeData(Stream stream)
+        protected void LogMessage(string message, params object[] args)
+        {
+            Console.WriteLine(message, args);
+            // added for Request 408
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log>"+message+"</log>", args);
+            }
+
+        }
+        public  void ProcessTrades(Stream stream)
+        //public void ProcessTrades(string url)
+        {
+            var lines = ReadTradeData(stream);
+            //var lines = ReadURLTradeData(url);
+            var trades = ParseTrades(lines);
+            StoreTrades(trades);
+        }
+        protected  IEnumerable<string> ReadTradeData(Stream stream)
         {
             LogMessage("INFO: ReadTradeData version 1");
             var tradeData = new List<string>();
@@ -25,7 +43,7 @@ namespace AbstractTrader
             return tradeData;
         }
 
-        protected override IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> tradeData)
+        protected  IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> tradeData)
         {
             LogMessage("INFO: ParseTrades version 1");
             var trades = new List<TradeRecord>();
